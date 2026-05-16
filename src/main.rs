c27f339948429aefd67b60398502349d0e34667e
Shakib448 <shakiba448@gmail.com>
2026-05-16 09:56:11 +0600

    Readme md generated

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,20 @@
+use trainpress::App;
+
 #[tokio::main]
-async fn main() {}
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "info, trainpress=debug".into()),
+        )
+        .init();
+
+    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let addr = format!("0.0.0.0:{}", port);
+
+    let app = App::new();
+
+    app.listen(&addr).await?;
+
+    Ok(())
+}
